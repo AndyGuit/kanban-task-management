b3793cf9ce91d067babdf16c89464366bb59748a
AndyGuit <totalslash@msn.com>
2023-08-04 20:40:41 +0300

    feat: showing active board name in header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,12 +1,17 @@
+import { useSelector } from 'react-redux';
 import HeaderBoardInfo from '../../components/Header/HeaderBoardInfo/HeaderBoardInfo';
 import HeaderLogo from '../../components/Header/HeaderLogo/HeaderLogo';
+import { RootState } from '../../store/index';
 import classes from './Header.module.scss';
 
 const Header = () => {
+  const boards = useSelector((state: RootState) => state.data.boards);
+  const activeBoardName = boards.filter(board => board.isActive)[0].name;
+
   return (
     <header className={`header ${classes.header}`}>
       <HeaderLogo />
-      <HeaderBoardInfo boardName="board name" />
+      <HeaderBoardInfo boardName={activeBoardName} />
     </header>
   );
 };
