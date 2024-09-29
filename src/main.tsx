3737dcaa9a7cb0576f9212939bce296253848215
AndyGuit <totalslash@msn.com>
2024-09-29 23:06:10 +0300

    refactor: move global styles to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import './styles/index.scss';
+import './app/styles/index.scss';
 import store from './store/index';
 import { Provider } from 'react-redux';
 
