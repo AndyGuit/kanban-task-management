9bc2a48f09b726309fc79162a7a0a338b9663591
AndyGuit <totalslash@msn.com>
2024-10-01 22:21:00 +0300

    refactor: sidebar to widgets/Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../layout/Header/Header';
-import Sidebar from '../layout/Sidebar/Sidebar';
 import Container from '../shared/layout/Container';
 import Modal from '../shared/ui/Modal/Modal';
 import ModalContentForm from '../components/ModalForms/ModalContentForm';
 import { UIActions, UISelectors } from './providers/StoreProvider';
 import { BoardDragDrop } from '../features/Board';
+import { Sidebar } from '../widgets/Sidebar';
 
 const App = () => {
   const dispatch = useDispatch();
