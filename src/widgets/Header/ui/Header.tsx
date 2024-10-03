367cda37165f23db83867b67211a6018c192541a
AndyGuit <totalslash@msn.com>
2024-10-03 21:16:50 +0300

    refactor: popupEditBoard to features/popup

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,25 +4,15 @@ import { ModalContent } from '../../../shared/types/modalFormContentTypes';
 import { DataSelectors, UIActions } from '../../../app/providers/StoreProvider';
 import Button from '../../../shared/ui/Button/Button';
 import { ButtonStyle } from '../../../shared/ui/Button/buttonStyles';
-import { ChevronDown, IconPopupDots } from '../../../shared/ui/Icons/Icons';
-import { Popover, PopoverContentPosition } from '../../../shared/ui/Popover';
+import { ChevronDown } from '../../../shared/ui/Icons/Icons';
 import classes from './Header.module.scss';
+import { PopupEditBoard } from '../../../features/Popup/ui/PopupEditBoard';
 
 export const Header = () => {
   const dispatch = useDispatch();
   const activeBoard = useSelector(DataSelectors.getActiveBoard);
 
   const title = activeBoard?.name || 'No Boards Found';
-
-  const onEditBoard = () => {
-    dispatch(UIActions.setModalContent(ModalContent.editBoard));
-    dispatch(UIActions.showModal());
-  };
-
-  const onDeleteBoard = () => {
-    dispatch(UIActions.setModalContent(ModalContent.confirmDeleteBoard));
-    dispatch(UIActions.showModal());
-  };
 
   const addNewTask = () => {
     dispatch(UIActions.setModalContent(ModalContent.addNewTask));
@@ -49,21 +39,7 @@ export const Header = () => {
             <Button onClick={addNewTask} styleClass={ButtonStyle.ADD_TASK}>
               +<span> Add New Task</span>
             </Button>
-            <Popover
-              direction={PopoverContentPosition.BOTTOM_LEFT}
-              trigger={
-                <Button styleClass={ButtonStyle.POPUP}>
-                  <IconPopupDots />
-                </Button>
-              }
-            >
-              <Button onClick={onEditBoard} styleClass={ButtonStyle.TEXT_PRIMARY}>
-                Edit Board
-              </Button>
-              <Button onClick={onDeleteBoard} styleClass={ButtonStyle.TEXT_WARNING}>
-                Delete Board
-              </Button>
-            </Popover>
+            <PopupEditBoard />
           </div>
         )}
       </div>
