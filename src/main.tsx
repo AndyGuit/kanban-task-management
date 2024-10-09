00aa2bf15110ceb55838d808486d9ec747fc50c5
AndyGuit <totalslash@msn.com>
2024-10-10 02:45:19 +0300

    feat: add steiger and aliases for absolute imports in vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import App from './app/App';
 import './app/styles/index.scss';
 import { DragDropContextProvider } from './app/providers/DragDropContext';
 import { StoreProvider } from './app/providers/StoreProvider';
-import Button from './shared/ui/Button/Button';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StoreProvider>
