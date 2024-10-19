7d70f40123657f564f8b63623dbd971e6b8de056
AndyGuit <totalslash@msn.com>
2024-10-20 01:13:45 +0300

    feat: add default user preferred color scheme

--- src/features/ThemeToggle/model/slices/theme-slice.ts
+++ src/features/ThemeToggle/model/slices/theme-slice.ts
@@ -3,11 +3,16 @@ import { AppTheme } from '../constants/appThemes';
 import { LocalStorageKeys } from 'src/shared/lib';
 
 const localStorageTheme = localStorage.getItem(LocalStorageKeys.Theme);
+const isDarkPreferred =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const defaultUserTheme = isDarkPreferred ? AppTheme.DARK : AppTheme.LIGHT;
 
 const themeSlice = createSlice({
   name: 'theme',
   initialState: {
-    theme: localStorageTheme || AppTheme.DARK,
+    theme: localStorageTheme || defaultUserTheme,
   },
   reducers: {
     toggleTheme: (state) => {
